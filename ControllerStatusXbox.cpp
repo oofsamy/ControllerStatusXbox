8044b4bb9ac0ca38a196c15be4d1be3a207cdf81
oofsamy <teamssglol@gmail.com>
2021-10-10 15:07:59 +0100

    Change state and generate notification function

--- ControllerStatusXbox.cpp
+++ ControllerStatusXbox.cpp
@@ -6,12 +6,84 @@ BAKKESMOD_PLUGIN(ControllerStatusXbox, "Alerts when your controller changes stat
 
 std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
 
+Controller* ControllerInstance = new Controller(1);
+
 void ControllerStatusXbox::onLoad()
 {
 	_globalCvarManager = cvarManager;
+	cvarManager->registerCvar("csx_enabled", "1", "Enable plugin", true, true, 1, true, 1, true);
+	gameWrapper->LoadToastTexture("xbox_logo", std::string(gameWrapper->GetDataFolderW().begin(), gameWrapper->GetDataFolderW().end()) + "/assets/xboxnoti.wav");
+
+	gameWrapper->HookEvent("Function Engine.GameViewportClient.Tick", [this](std::string eventName) {
+		if (ControllerInstance->IsConnected()) {
+			switch (ControllerInstance->SetBatteryInfo(0, BATTERY_DEVTYPE_GAMEPAD, &ControllerInstance->BatteryInfo)) {
+				switch (ControllerInstance->BatteryInfo.BatteryLevel) {
+					case BATTERY_LEVEL_EMPTY:
+						if (StateOfController != CRITICAL) {
+							StateOfController = CRITICAL;
+						}
+						break;
+					case BATTERY_LEVEL_LOW:
+						if (StateOfController != LOW) {
+							StateOfController = LOW;
+						}
+						break;
+					case BATTERY_LEVEL_MEDIUM:
+						if (StateOfController != MEDIUM) {
+							StateOfController = MEDIUM;
+						}
+						break;
+					case BATTERY_LEVEL_FULL:
+						if (StateOfController != FULL) {
+							StateOfController = FULL;
+						}
+						break;
+				}
+			}
+		} else {
+			if (StateOfController != DISCONNECTED) {
+				StateOfController = DISCONNECTED;
+			}
+		}
+	});
 
 }
 
-void ControllerStatusXbox::onUnload()
-{
+void ControllerStatusXbox::onUnload() {
+	delete ControllerInstance;
+}
+
+void ControllerStatusXbox::GenerateNotification(int StateType) {
+	CVarWrapper EnabledCVar = cvarManager->getCvar("csx_enabled");
+	if (EnabledCVar.getBoolValue()) {
+		std::string Title = "Xbox Controller Status";
+
+		if (StateType == 1) {
+			std::string Message = "Controller has been disconnected";
+			gameWrapper->Toast(Title, Message, "xbox_logo", 3.5f, ToastType_Info, 500.f, 75.f); //float width = 290.f, float height = 60.f last 2 parameters
+		}
+		else if (StateType == 2) {
+			std::string Message = "Controller has been connected";
+			gameWrapper->Toast(Title, Message, "xbox_logo", 3.5f, ToastType_Info, 500.f, 75.f);
+		}
+		else if (StateType == 3) {
+			std::string Message = "Controller battery is now at critical\nStart charging it now!";
+			gameWrapper->Toast(Title, Message, "xbox_logo", 3.5f, ToastType_Error, 500.f, 75.f);
+		}
+		else if (StateType == 4) {
+			std::string Message = "Controller battery is now at Low";
+			gameWrapper->Toast(Title, Message, "xbox_logo", 3.5f, ToastType_Warning, 500.f, 75.f);
+		}
+		else if (StateType == 5) {
+			std::string Message = "Controller battery is now at Medium";
+			gameWrapper->Toast(Title, Message, "xbox_logo", 3.5f, ToastType_OK, 500.f, 75.f);
+		}
+		else if (StateType == 7) {
+			std::string Message = "Controller battery is now Full\nFeel free to unplug your controller!";
+			gameWrapper->Toast(Title, Message, "xbox_logo", 3.5f, ToastType_OK, 500.f, 75.f);
+		}
+		else {
+			gameWrapper->Toast(Title, "No valid argument was given", "xbox_logo", 3.5f, ToastType_Error, 350.f, 75.f);
+		}
+	}
 }
--- ControllerStatusXbox.h
+++ ControllerStatusXbox.h
@@ -4,6 +4,8 @@
 #include "bakkesmod/plugin/pluginwindow.h"
 #include "bakkesmod/plugin/PluginSettingsWindow.h"
 
+#include "ControllerWrapper.h"
+
 #include "version.h"
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
@@ -17,5 +19,19 @@ class ControllerStatusXbox: public BakkesMod::Plugin::BakkesModPlugin, public Ba
 	void RenderSettings() override;
 	std::string GetPluginName() override;
 	void SetImGuiContext(uintptr_t ctx) override;
+
+	void GenerateNotification(int StateType);
+
+	int StateOfController = DISCONNECTED;
+
+	enum States {
+		DISCONNECTED = 0x01,
+		CONNECTED = 0x02,
+		CRITICAL = 0x03,
+		LOW = 0x04,
+		MEDIUM = 0x05,
+		FULL = 0x06,
+		CHARGING = 0x07,
+	};
 };
 
