6dcc16390614d3f61c097423e5985f16a8d9018d
oofsamy <teamssglol@gmail.com>
2021-10-10 14:27:10 +0100

    Inital commit

--- ControllerStatusXboxGUI.cpp
+++ ControllerStatusXboxGUI.cpp
@@ -1,7 +1,6 @@
 #include "pch.h"
 #include "ControllerStatusXbox.h"
 
-/* Plugin Settings Window code here
 std::string ControllerStatusXbox::GetPluginName() {
 	return "ControllerStatusXbox";
 }
@@ -10,72 +9,6 @@ void ControllerStatusXbox::SetImGuiContext(uintptr_t ctx) {
 	ImGui::SetCurrentContext(reinterpret_cast<ImGuiContext*>(ctx));
 }
 
-// Render the plugin settings here
-// This will show up in bakkesmod when the plugin is loaded at
-//  f2 -> plugins -> ControllerStatusXbox
 void ControllerStatusXbox::RenderSettings() {
-	ImGui::TextUnformatted("ControllerStatusXbox plugin settings");
+	ImGui::TextUnformatted("This plugin uses BakkesModSDK's notification system to alert you whenever your controller is at High/Medium/Low/Critical battery level and when it disconnects/connects");
 }
-*/
-
-/*
-// Do ImGui rendering here
-void ControllerStatusXbox::Render()
-{
-	if (!ImGui::Begin(menuTitle_.c_str(), &isWindowOpen_, ImGuiWindowFlags_None))
-	{
-		// Early out if the window is collapsed, as an optimization.
-		ImGui::End();
-		return;
-	}
-
-	ImGui::End();
-
-	if (!isWindowOpen_)
-	{
-		cvarManager->executeCommand("togglemenu " + GetMenuName());
-	}
-}
-
-// Name of the menu that is used to toggle the window.
-std::string ControllerStatusXbox::GetMenuName()
-{
-	return "ControllerStatusXbox";
-}
-
-// Title to give the menu
-std::string ControllerStatusXbox::GetMenuTitle()
-{
-	return menuTitle_;
-}
-
-// Don't call this yourself, BM will call this function with a pointer to the current ImGui context
-void ControllerStatusXbox::SetImGuiContext(uintptr_t ctx)
-{
-	ImGui::SetCurrentContext(reinterpret_cast<ImGuiContext*>(ctx));
-}
-
-// Should events such as mouse clicks/key inputs be blocked so they won't reach the game
-bool ControllerStatusXbox::ShouldBlockInput()
-{
-	return ImGui::GetIO().WantCaptureMouse || ImGui::GetIO().WantCaptureKeyboard;
-}
-
-// Return true if window should be interactive
-bool ControllerStatusXbox::IsActiveOverlay()
-{
-	return true;
-}
-
-// Called when window is opened
-void ControllerStatusXbox::OnOpen()
-{
-	isWindowOpen_ = true;
-}
-
-// Called when window is closed
-void ControllerStatusXbox::OnClose()
-{
-	isWindowOpen_ = false;
-}
-*/
